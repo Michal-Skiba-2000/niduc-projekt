ca2ce3c82286bf4a7b5ea5261a4716438bc55453
michal.skiba.2000@gmail.com <michal.skiba.2000@gmail.com>
2021-06-21 22:05:07 +0200

    Final version

--- ImageToBits.py
+++ ImageToBits.py
@@ -1,25 +1,68 @@
 from PIL import Image
+from math import pow
 
-image = Image.open("buchal.jpg")
-#image.show()
 
-def pixel_to_array(pixel):
-    bin_arr = bin(pixel)[2:]
-    return_arr = []
+class ImageBitParser:
+    @staticmethod
+    def number_to_bit_array(number, length):
+        bin_arr = bin(number)[2:]
+        return_arr = []
 
-    for bit in bin_arr:
-        return_arr.append(bit)
+        for bit in bin_arr:
+            return_arr.append(int(bit))
 
-    num = 8 - len(return_arr)
+        num = length - len(return_arr)
+        for i in range(0, num):
+            return_arr.insert(0, 0)
 
-    for i in range(0, num):
-        return_arr.insert(0, 0)
+        return return_arr
 
-    return(return_arr)
+    @classmethod
+    def parse_img_to_bit_array(cls, image_name):
+        image = Image.open(image_name)
+        bit_array = []
+        bit_array += cls.number_to_bit_array(image.height, 16)
+        bit_array += cls.number_to_bit_array(image.width, 16)
 
-for y in range(image.height):
-    for x in range(image.width):
-        pixels = image.getpixel((x,y))
-        print(pixels)
-        for pixel in pixels:
-            print(pixel_to_array(pixel))
+        for y in range(image.height):
+            for x in range(image.width):
+                pixels = image.getpixel((x, y))
+                for pixel in pixels:
+                    bit_array += cls.number_to_bit_array(pixel, 8)
+
+        return bit_array
+
+    @staticmethod
+    def bit_array_to_number(bit_array):
+        i = 0
+        number = 0
+        for bit in reversed(bit_array):
+            number += bit * pow(2, i)
+            i += 1
+
+        return int(number)
+
+    @classmethod
+    def save_image_from_bit_array(cls, image_name, bit_array):
+        height = cls.bit_array_to_number(bit_array[:16])
+        bit_array = bit_array[16:]
+        width = cls.bit_array_to_number(bit_array[:16])
+        bit_array = bit_array[16:]
+        image = Image.new('RGB', (width, height), 'red')
+
+        i = 0
+
+        for y in range(height):
+            for x in range(width):
+                pixel = []
+                for j in range(3):
+                    pixel.append(cls.bit_array_to_number([bit_array[0+((i+j)*8)],bit_array[1+((i+j)*8)],
+                                                          bit_array[2+((i+j)*8)], bit_array[3+((i+j)*8)],
+                                                          bit_array[4+((i+j)*8)], bit_array[5+((i+j)*8)],
+                                                          bit_array[6+((i+j)*8)], bit_array[7+((i+j)*8)]]))
+
+                pixel = (pixel[0], pixel[1], pixel[2])
+                image.putpixel((x, y), pixel)
+                i += 3
+
+        image.save(image_name)
--- main.py
+++ main.py
@@ -1,113 +1,187 @@
-from PIL import Image
-
-image = Image.open("buchal.jpg")
-image.show()
-
-for y in range(image.height):
-    for x in range(image.width):
-        print(image.getpixel((x,y)))
-
-#an_int = 15
-#a_bytes = str(bin(an_int))[2:]
-#print(a_bytes)
+import csv
+import hamming_codec
 import random
+from reedsolo import RSCodec
 
-class Channel():
+from ImageToBits import ImageBitParser
 
-    def __init__(self, s, r, noise_rate = 0.1):
-        self.sender = s;
+TRIPLE = "triple"
+HAMMING = "hamming"
+SALOMON = "salomon"
+SALOMON_DEPTH = 4
+MULTIPLIER = 5
+
+
+class Channel:
+    def __init__(self, s, r, noise_rate):
+        self.sender = s
         self.receiver = r
         self.noise_rate = noise_rate
 
     def communicate(self):
-        msg = self.sender.get_encoded_msg()
-        msg = self._noise(msg)
-        self.receiver.receive_msg(msg)
+        for packet in self.sender.get_encoded_msg():
+            packet = self._noise(packet)
+            self.receiver.receive_packet(packet)
 
-    def _noise(self, msg):
+    def _noise(self, packet):
+        new_packet = []
 
-        new_msg = []
-
-        for bit in msg:
-            rand_int = random.randint(1, 100001)
-            if rand_int <= 100001*self.noise_rate:
+        for bit in packet:
+            rand_int = random.randint(1, 100000)
+            limit = 100000*self.noise_rate
+            if rand_int <= limit:
+                # print('rand_int: {}, limit: {}, noise_rate: {}'.format(rand_int, limit, self.noise_rate))
                 if bit == 0:
-                    new_msg.append(1)
+                    new_packet.append(1)
+                elif bit == 1:
+                    new_packet.append(0)
                 else:
-                    new_msg.append(0)
+                    raise
             else:
-                new_msg.append(bit)
+                new_packet.append(bit)
 
-        print("Noised message:   " + str(new_msg))
-
-        return new_msg
+        return new_packet
 
 
-class Sender():
-
-    def __init__(self, encode_method):
-        msg = []
-        for i in range(8):
-            msg.append(random.randint(0, 1))
-
+class Sender:
+    def __init__(self, msg, encode_method, bits_per_packet):
         self.msg = msg
         self.encode_method = encode_method
+        self.bits_per_packet = bits_per_packet
+        self.counter = 0
+        self.rsc = RSCodec(SALOMON_DEPTH)
 
     def get_encoded_msg(self):
+        while len(self.msg) >= (self.counter+1) * self.bits_per_packet:
+            if self.encode_method == TRIPLE:
+                yield self._get_triple_packet()
+            elif self.encode_method == HAMMING:
+                yield self._get_hamming_packet()
+            elif self.encode_method == SALOMON:
+                yield self._get_salomon_packet()
+            else:
+                raise
 
-        msg = None
-        if self.encode_method == "triple": msg = self._triple_msg()
+    def _get_triple_packet(self):
+        packet_start = self.counter * self.bits_per_packet
+        packet_end = (self.counter+1) * self.bits_per_packet
+        packet = self.msg[packet_start:packet_end]
+        encoded_packet = []
 
-        return msg
+        for bit in packet:
+            for i in range(MULTIPLIER):
+                encoded_packet.append(bit)
 
-    def _triple_msg(self):
-        encoded_msg = []
+        self.counter += 1
+        return encoded_packet
 
-        for bit in self.msg:
-            for i in range(3):
-                encoded_msg.append(bit)
+    def _get_hamming_packet(self):
+        packet_start = self.counter * self.bits_per_packet
+        packet_end = (self.counter + 1) * self.bits_per_packet
+        packet = self.msg[packet_start:packet_end]
+        number = ImageBitParser.bit_array_to_number(packet)
+        encoded_packet = [int(bit) for bit in hamming_codec.encode(number, self.bits_per_packet)]
+        self.counter += 1
+        return encoded_packet
 
-        print("Encoded message:  " + str(encoded_msg))
-        return encoded_msg
+    def _get_salomon_packet(self):
+        packet_start = self.counter * self.bits_per_packet
+        packet_end = (self.counter + 1) * self.bits_per_packet
+        packet = self.msg[packet_start:packet_end]
+        encoded_value = self.rsc.encode(packet)
+        for number in encoded_value[self.bits_per_packet:]:
+            packet += ImageBitParser.number_to_bit_array(number, 8)
+        self.counter += 1
+        return packet
 
 
-    def print_msg(self):
-        print("Message to send:  " + str(self.msg))
-
-
-class Receiver():
-
-    def __init__(self, decode_method):
-        self.received_msg = ""
+class Receiver:
+    def __init__(self, decode_method, bits_per_packet):
+        self.msg = []
         self.decode_method = decode_method
+        self.bits_per_packet = bits_per_packet
+        self.rsc = RSCodec(SALOMON_DEPTH)
 
-    def receive_msg(self, msg):
+    def receive_packet(self, packet):
+        if self.decode_method == TRIPLE:
+            self._decode_triple_packet(packet)
+        elif self.decode_method == HAMMING:
+            self._decode_hamming_packet(packet)
+        elif self.decode_method == SALOMON:
+            self._decode_salomon_packet(packet)
 
-        if self.decode_method == "triple": msg = self._triple_decode(msg)
+    def _decode_triple_packet(self, packet):
+        for i in range(0, len(packet), MULTIPLIER):
+            zero = 0
+            one = 0
+            for j in range(3):
+                if packet[i+j] == 0:
+                    zero += 1
+                elif packet[i+j] == 1:
+                    one += 1
+                else:
+                    raise
 
-        self.received_msg = msg
+            if zero > one:
+                self.msg.append(0)
+            else:
+                self.msg.append(1)
 
-    def print_received_msg(self):
-        print("Received message: " + str(self.received_msg))
+    def _decode_hamming_packet(self, packet):
+        number = ImageBitParser.bit_array_to_number(packet)
+        decoded_packet = [int(bit) for bit in hamming_codec.decode(number, len(packet))]
+        self.msg += decoded_packet
 
-    def _triple_decode(self, msg):
+    def _decode_salomon_packet(self, packet):
+        decoded_packet = [bit for bit in packet[:self.bits_per_packet]]
+        packet = packet[self.bits_per_packet:]
 
-        length = len(msg)
+        for i in range(0, len(packet), 8):
+            bit_array = [packet[i], packet[i+1], packet[i+2], packet[i+3],
+                         packet[i+4], packet[i+5], packet[i+6], packet[i+7]]
+            number = ImageBitParser.bit_array_to_number(bit_array)
+            decoded_packet.append(number)
 
-        new_msg = []
+        try:
+            decoded_packet = self.rsc.decode(bytearray(decoded_packet))
+        except Exception:
+            decoded_packet = [decoded_packet[:self.bits_per_packet]]
 
-        for i in range(0, length, 3):
-            if msg[i] == msg[i+1] or msg[i] == msg[i+2]: new_msg.append(msg[i])
-            elif msg[i+1] == msg[i+2]: new_msg.append(msg[i+2])
+        decoded_packet = [bit for bit in decoded_packet[0]]
+        self.msg += decoded_packet
 
-        return new_msg
 
-coding_method = "triple"
+def compare_two_arrays(csw_writer, decode_method, bits, i, arr1, arr2):
+    if len(arr1) != len(arr2):
+        raise
 
-s = Sender(coding_method)
-r = Receiver(coding_method)
-ch = Channel(s, r)
+    counter = 0
+    for j in range(len(arr1)):
+        if arr1[j] != arr2[j]:
+            counter += 1
 
-s.print_msg()
-ch.communicate()
-r.print_received_msg()
+    csw_writer.writerow([decode_method, bits, i, counter/len(arr1)*100])
+
+
+
+def foo():
+    with open('data.csv', mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['Metoda dekodowania', 'Ilosc bitow w pakiecie', 'Prawdobodopienstwo zmiany bitu(%)', 'Zmiana wiadomosci(%)'])
+        for decode_method in [TRIPLE, HAMMING, SALOMON]:
+            for bits in [4, 8]:
+                for i in [0, 1, 3, 5, 10, 20, 30, 40, 50]:
+                    image_bit_array = ImageBitParser.parse_img_to_bit_array('input.jpg')
+                    size = image_bit_array[:32]
+                    image_bit_array = image_bit_array[32:]
+                    sender = Sender(image_bit_array, decode_method, bits)
+                    receiver = Receiver(decode_method, bits)
+                    channel = Channel(sender, receiver, i/100)
+                    channel.communicate()
+                    ImageBitParser.save_image_from_bit_array('out_{}_{}.jpg'.format(decode_method, i), size+receiver.msg)
+                    compare_two_arrays(csv_writer, decode_method, bits, i, sender.msg, receiver.msg)
+                    print('Ended communication for {} method, with {}% of changing single bit, with {} bits per packet.'.format(decode_method, i, bits))
+
+
+if __name__ == '__main__':
+    foo()
